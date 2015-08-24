576a4fa028c788138eee9a21198ef0837008c0ef
Crucq <info@pietercrucq.nl>
2015-08-24 20:22:27 +0200

    new theme files for Intranet Agenda 1.1

--- template.php
+++ template.php
@@ -11,6 +11,146 @@
  * for more information on this topic.
  */
 
+// START TEMPLATE FUNCTIONS FOR IISH AGENDA
+/*
+ * Adds reservation number (node id) and cancelled value to template
+ */
+function iisg_preprocess_node(&$variables, $hook){
+    if($variables["type"] == "event"){
+        $variables["reservation_number"] =  t("Reserveringsnummer: ")."#".$variables["nid"];
+        //dpm($variables);
+        if($variables['field_event_status'][0]['value'] == 'cancelled')  $variables["cancelled"] = t("Geannuleerd");
+
+        if (drupal_valid_path('clone/'.$variables['nid'])) {
+            $variables["clone"] = true;
+        }else{
+            $variables["clone"] = false;
+        }
+    }
+}
+
+/*
+ * Makes changes to event views output
+ */
+function iisg_views_pre_render(&$view) {
+
+    switch ($view->name) {
+        case 'today_events':
+            foreach($view->result as $r => &$result) {
+                _cancelcheck($result);
+            }
+            break;
+
+        case 'upcoming_events':
+
+            // My events display should show any status
+            if($view->current_display !== "page_1"){
+                foreach($view->result as $r => &$result) {
+                    _cancelcheck($result);
+                }
+            }
+
+            break;
+
+        case 'calendar':
+            foreach($view->result as $r => &$result) {
+                _cancelcheck($result);
+                $status = issetor($result->field_field_event_status[0]['raw']['value'],false);
+                if(!$status || $status == "new"){
+                    $result->node_title = "- nog niet bevestigd -";
+                }
+            }
+            break;
+
+        case 'catering':
+            foreach($view->result as $r => &$result) {
+                _cancelcheck($result);
+                if(isset($result->field_field_note[0]) && isset($result->field_field_event_room[0])){
+                    $note = '<div class="messagepop pop">'.nl2br($result->field_field_note[0]["rendered"]["#markup"]).'</div>';
+                    $markup = $note.'<a href="#" class="messagepop-link">opmerking!</a><br>'.$result->field_field_event_room[0]["rendered"]["#markup"];;
+                    $result->field_field_event_room[0]["rendered"] = array('#markup'=>$markup,'#access'=>TRUE);
+                }
+            }
+            break;
+    }
+}
+
+/*
+ * Adds class to colorize events in calendar based on int/ext field value
+ */
+function iisg_preprocess_calendar_item(&$vars) {
+    $view = $vars['view'];
+    if($view->name == "calendar"){
+        $item = $vars["item"];
+        $vars["item"]->class = "item ".$item->row->field_field_internal_external[0]['raw']['value'];
+    }
+}
+
+
+/*
+ * Changes date titles of calendar views
+ */
+function iisg_date_nav_title($params) {
+    $granularity = $params['granularity'];
+    $view = $params['view'];
+    $date_info = $view->date_info;
+    $link = !empty($params['link']) ? $params['link'] : FALSE;
+    $format = !empty($params['format']) ? $params['format'] : NULL;
+    switch ($granularity) {
+        case 'year':
+            $title = $date_info->year;
+            $date_arg = $date_info->year;
+            break;
+        case 'month':
+            $format = !empty($format) ? $format : (empty($date_info->mini) ? 'F Y' : 'F Y');
+            $title = date_format_date($date_info->min_date, 'custom', $format);
+            $date_arg = $date_info->year .'-'. date_pad($date_info->month);
+            break;
+        case 'day':
+            $format = !empty($format) ? $format : (empty($date_info->mini) ? 'l j F Y' : 'l j F Y');
+            $title = date_format_date($date_info->min_date, 'custom', $format);
+            $date_arg = $date_info->year .'-'. date_pad($date_info->month) .'-'. date_pad($date_info->day);
+            break;
+        case 'week':
+            $format = !empty($format) ? $format : (empty($date_info->mini) ? 'W, Y' : 'W, Y');
+            $title = t('Week @date', array('@date' => date_format_date($date_info->min_date, 'custom', $format)));
+            $date_arg = $date_info->year .'-W'. date_pad($date_info->week);
+            break;
+    }
+    if (!empty($date_info->mini) || $link) {
+        $attributes = array('title' => t('View full page month'));
+        $url = date_pager_url($view, $granularity, $date_arg, TRUE);
+        return l($title, $url, array('attributes' => $attributes));
+    }
+    else {
+        return $title;
+    }
+}
+
+/*
+ * Add cancelled div for cancelled events
+ */
+function _cancelcheck(&$result){
+    if(isset($result->field_field_event_status) && isset($result->field_field_event_status[0])){
+        if( $result->field_field_event_status[0]['raw']['value'] == 'cancelled'){
+            $result->field_field_event_status[0]['rendered'] = "<span class='cancelled'>geannuleerd</span>";
+        }else{
+            $result->field_field_event_status[0]['rendered'] = "";
+        }
+
+    }else if(isset($result->field_field_canceled)){
+
+        // TODO: has to be removed when status field becomes the only one
+        if( $result->field_field_canceled[0]['raw']['value'] == 1){
+            $result->field_field_canceled[0]['rendered'] = "<span class='cancelled'>geannuleerd</span>";
+        }else{
+            $result->field_field_canceled[0]['rendered'] = "";
+        }
+    }
+}
+// END TEMPLATE FUNCTIONS FOR IISH AGENDA
+
+
 
 /**
  * Return a themed breadcrumb trail. (Taken from Zen)
@@ -384,5 +524,8 @@ function iisg_pager_last($variables) {
 function iisg_preprocess_user_profile_category(&$variables) {
   $variables['title'] = t($variables['title']);
 }
+
+
+
 
 
