e324d1d2277829ace5ccf27a59483da08123d22a
crucq <pieter@pieters-mbp.home>
2016-12-19 15:15:57 +0100

    changes for calendar flex view

--- template.php
+++ template.php
@@ -35,6 +35,7 @@ function iisg_preprocess_node(&$variables, $hook) {
  * Makes changes to event views output
  */
 function iisg_views_pre_render(&$view) {
+
 	switch ($view->name) {
 		case 'today_events':
 			foreach ($view->result as $r => &$result) {
@@ -42,17 +43,14 @@ function iisg_views_pre_render(&$view) {
 			}
 			break;
 		case 'upcoming_events':
-
-            // My events display should show any status
+        case 'upcoming_flex':
             $ics_icon = "/".drupal_get_path("module","iishagenda")."/images/ics-icon.gif";
-
             foreach ($view->result as $r => &$result) {
-
                 if ($view->current_display !== "page_1") _cancelcheck($result); // My events display should show any status
                 $result->field_field_event_date[0]['rendered']["#markup"] = "<a class='ics-link' href='/ics/".$result->nid."' title='Download .ics'><img src='".$ics_icon."' alt='ics'></a>".$result->field_field_event_date[0]['rendered']["#markup"];
             }
 
-			break;
+            break;
 		case 'calendar':
 			foreach ($view->result as $r => &$result) {
 				_cancelcheck($result);
@@ -103,7 +101,7 @@ function _cancelcheck(&$result) {
  */
 function iisg_preprocess_calendar_item(&$vars) {
 	$view = $vars['view'];
-	if ($view->name == "calendar") {
+	if ($view->name == "calendar" || $view->name == "calendar_flex") {
 		$item = $vars["item"];
 		if(isset($vars["item"]->class)){
             $vars["item"]->class .= " item " . $item->row->field_field_internal_external[0]['raw']['value'];
